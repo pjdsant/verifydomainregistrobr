924b800cff343c0df6e45312745ee33863918325
pjdsant@gmail.com <pjdsant@gmail.com>
2019-08-16 15:34:20 -0300

    Cleaning code

--- Program.cs
+++ Program.cs
@@ -31,27 +31,11 @@ namespace VerifyDomainRegistroBr
             ThreadOne();
             Console.ReadLine();
 
-            new Thread(ThreadTwo).Start();
-            ThreadTwo();
-            Console.ReadLine();
-
-            new Thread(ThreadThree).Start();
-            ThreadThree();
-            Console.ReadLine();
-
-            new Thread(ThreadFour).Start();
-            ThreadFour();
-            Console.ReadLine();
-
-            new Thread(ThreadFive).Start();
-            ThreadFive();
-            Console.ReadLine();
-
             void ThreadOne()
             {
                 try
                 {
-                    for (int i = 704; i <= 2703; i++) //defina aqui o tamanho da sequencia gerada
+                    for (int i = 27; i <= 703; i++) //defina aqui o tamanho da sequencia gerada
                     {
                         jx = i;
                         WebRequest request = WebRequest.Create("https://rdap.registro.br/domain/" + IntToAlpha(i) + strDomain);
@@ -71,106 +55,6 @@ namespace VerifyDomainRegistroBr
                 }
             }
 
-            void ThreadTwo()
-            {
-
-
-                try
-                {
-                    for (int i = 2704; i <= 4703; i++) //defina aqui o tamanho da sequencia gerada
-                    {
-                        jx = i;
-                        WebRequest request = WebRequest.Create("https://rdap.registro.br/domain/" + IntToAlpha(i) + strDomain);
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                        if (response.StatusCode != HttpStatusCode.OK)
-                        {
-                            Console.WriteLine(i);
-                            Console.WriteLine(IntToAlpha(i) + " = " + response.StatusCode);
-                        }
-                        response.Close();
-                    }
-                }
-                catch (WebException we)
-                {
-                    Console.WriteLine(IntToAlpha(jx) + " = " + ((HttpWebResponse)we.Response).StatusCode);
-                }
-            }
-
-            void ThreadThree()
-            {
-
-                try
-                {
-                    for (int i = 4704; i <= 6703; i++) //defina aqui o tamanho da sequencia gerada
-                    {
-                        jx = i;
-                        WebRequest request = WebRequest.Create("https://rdap.registro.br/domain/" + IntToAlpha(i) + strDomain);
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                        if (response.StatusCode != HttpStatusCode.OK)
-                        {
-                            Console.WriteLine(i);
-                            Console.WriteLine(IntToAlpha(i) + " = " + response.StatusCode);
-                        }
-                        response.Close();
-                    }
-                }
-                catch (WebException we)
-                {
-                    Console.WriteLine(IntToAlpha(jx) + " = " + ((HttpWebResponse)we.Response).StatusCode);
-                }
-            }
-
-            void ThreadFour()
-            {
-
-                try
-                {
-                    for (int i = 6704; i <= 8703; i++) //defina aqui o tamanho da sequencia gerada
-                    {
-                        jx = i;
-                        WebRequest request = WebRequest.Create("https://rdap.registro.br/domain/" + IntToAlpha(i) + strDomain);
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                        if (response.StatusCode != HttpStatusCode.OK)
-                        {
-                            Console.WriteLine(i);
-                            Console.WriteLine(IntToAlpha(i) + " = " + response.StatusCode);
-                        }
-                        response.Close();
-                    }
-                }
-                catch (WebException we)
-                {
-                    Console.WriteLine(IntToAlpha(jx) + " = " + ((HttpWebResponse)we.Response).StatusCode);
-                }
-            }
-
-            void ThreadFive()
-            {
-
-                try
-                {
-                    for (int i = 8704; i <= 10703; i++) //defina aqui o tamanho da sequencia gerada
-                    {
-                        jx = i;
-                        WebRequest request = WebRequest.Create("https://rdap.registro.br/domain/" + IntToAlpha(i) + strDomain);
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                        if (response.StatusCode != HttpStatusCode.OK)
-                        {
-                            Console.WriteLine(i);
-                            Console.WriteLine(IntToAlpha(i) + " = " + response.StatusCode);
-                        }
-                        response.Close();
-                    }
-                }
-                catch (WebException we)
-                {
-                    Console.WriteLine(IntToAlpha(jx) + " = " + ((HttpWebResponse)we.Response).StatusCode);
-                }
-            }
         }
     }
 }
